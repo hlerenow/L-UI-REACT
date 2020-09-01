093841558c466d345e6c981949beb94238425d8c
lei.he <helei@tinman.cn>
2020-09-01 15:14:39 +0800

    feat: add demo file

--- src/components/Button/__demo__/index.tsx
+++ src/components/Button/__demo__/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import Button from '@/src/components/Button/index';
+// eslint-disable-next-line no-unused-vars
+import { Button, ButtonType, ButtonSize } from '@/src/components/Button/index';
 import './index.scss';
 
 type StateType = {
@@ -12,7 +13,7 @@ class ButtonDemo extends React.Component<any, StateType> {
     super(props);
     this.state = {
       refs: React.createRef(),
-      types: ['default', 'primary', 'success', 'info', 'warning', 'danger'],
+      types: ['default', 'primary', 'success', 'info', 'warn', 'danger'],
     };
   }
 
@@ -23,36 +24,37 @@ class ButtonDemo extends React.Component<any, StateType> {
   render() {
     const { onClick } = this;
     const { refs, types } = this.state;
+    const sizeTypes: ButtonSize[] = ['medium', 'small', 'mini', 'small'];
     return (
       <div>
         <h2>Button</h2>
         <div className="item-box">
           {
-            types.map((type) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button type={type}>{type}</Button></div>))
+            types.map((type: ButtonType) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button type={type}>{type}</Button></div>))
           }
         </div>
         {/* plain */}
         <div className="item-box">
           {
-            types.map((type) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button plain type={type}>{type}</Button></div>))
+            types.map((type: ButtonType) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button plain type={type}>{type}</Button></div>))
           }
         </div>
         {/* round */}
         <div className="item-box">
           {
-            types.map((type) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button round type={type}>{type}</Button></div>))
+            types.map((type: ButtonType) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button round type={type}>{type}</Button></div>))
           }
         </div>
         {/* round and plain */}
         <div className="item-box">
           {
-            types.map((type) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button round plain type={type}>{type}</Button></div>))
+            types.map((type: ButtonType) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button round plain type={type}>{type}</Button></div>))
           }
         </div>
         {/* size */}
         <div className="item-box">
           {
-            types.map((type, index) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button size={['', 'medium', 'small', 'mini', 'small', 'medium'][index]} type={type}>{type}</Button></div>))
+            types.map((type: ButtonType, index) => (<div className="item" key={type} onClick={onClick} ref={refs}><Button size={sizeTypes[index]} type={type}>{type}</Button></div>))
           }
         </div>
       </div>
